88df196b1ea40da22642f296935a8c6a84f07931
Omar28Radiance <omarin0a@hotmail.com>
2022-09-14 12:42:49 -0500

    js preguntar por el metodo usado

--- src/index.js
+++ src/index.js
@@ -2,14 +2,23 @@ const openBtn = document.querySelector('button');
 const closeBtn = document.querySelector('#close');
 const modal = document.querySelector('.modal');
 
+// OPEN MODAL
 openBtn.addEventListener('click', openModal);
 
 function openModal(){
     modal.style.display = 'grid';
 }
 
+
+//CLOSE MODAL
 closeBtn.addEventListener('click', closeModal);
 
 function closeModal(){
     modal.style.display = 'none';
 }
+
+modal.addEventListener('click', function(e){
+    if(e.target.classList.contains('modal')){
+        closeModal();
+    }
+});
